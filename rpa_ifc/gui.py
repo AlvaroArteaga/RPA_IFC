1dccc019b305096d630305d969cae86a12b86f85
AlvaroArteaga <arteaga.alvaro@outlook.com>
2022-11-23 20:00:53 -0300

    2133

--- rpa_ifc/gui.py
+++ rpa_ifc/gui.py
@@ -22,6 +22,12 @@ from kivy.core.window import Window
 from kivymd.uix.pickers import MDDatePicker
 from kivymd.uix.label import MDLabel
 from kivymd.uix.selectioncontrol import MDSwitch
+from kivy.metrics import dp
+from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.menu import MDDropdownMenu
+import datetime
+
+
 
 Window.size = (470,700)
 
@@ -89,7 +95,7 @@ MDBoxLayout:
                     id: meses
                     text: "Seleccione los meses a descargar"
                     halign: "left"
-                    font_size: 15
+                    font_size: 14
 
                 MDGridLayout:
                     id: gridsw
@@ -101,13 +107,101 @@ MDBoxLayout:
                     MDLabel:
                         text: "Un solo mes"
                         halign: "left"
-                        width:150
+                        width:300
                         size_hint_x:None
-                        font_size: 15
+                        font_size: 12
+                        italic: True
                     
                     MDSwitch:
+                        id:messw
                         width: dp(64)
                         active: True
+                        on_active: app.test_state()
+
+                    MDLabel:
+                        id: variosLB
+                        text: "Mismo periodo para los procesos seleccionados"
+                        halign: "left"
+                        width:300
+                        size_hint_x:None
+                        font_size: 12
+                        italic: True
+    
+                    MDSwitch:
+                        id:varios
+                        width: dp(64)
+                        active: True
+                        opacity: 1
+
+                MDGridLayout:
+                    id: gridsw2
+                    cols: 4
+                    row_force_default:True
+                    row_default_height:25
+                    
+                    MDLabel:
+                        id: mdlabelano
+                        text: "Seleccione el AÑO"
+                        halign: "left"
+                        width:145
+                        size_hint_x:None
+                        font_size: 13
+                        bold: True
+                    
+                    MDDropDownItem:
+                        id: drop_item
+                        pos_hint: {'center_x': .5, 'center_y': .5}
+                        text: 'item 0'
+                        on_release: app.menu.open()
+                        font_size: 13
+
+                    MDLabel:
+                        id: mdlabelmes
+                        text: "y el MES"
+                        halign: "right"
+                        width:90
+                        size_hint_x:None
+                        font_size: 13
+                        bold: True
+                    
+                    MDDropDownItem:
+                        id: drop_item2
+                        pos_hint: {'center_x': .5, 'center_y': .5}
+                        text: 'item 0'
+                        on_release: app.menu2.open()
+                        font_size: 13
+
+                    MDLabel:
+                        id: mdlabelano_fn
+                        text: "Seleccione el AÑO Final"
+                        halign: "left"
+                        width:145
+                        size_hint_x:None
+                        font_size: 13
+                        bold: True
+                    
+                    MDDropDownItem:
+                        id: drop_item_fn
+                        pos_hint: {'center_x': .5, 'center_y': .5}
+                        text: 'item 0'
+                        on_release: 
+                        font_size: 13
+
+                    MDLabel:
+                        id: mdlabelmes_fn
+                        text: "y el MES Final"
+                        halign: "right"
+                        width:90
+                        size_hint_x:None
+                        font_size: 13
+                        bold: True
+                    
+                    MDDropDownItem:
+                        id: drop_item2_fn
+                        pos_hint: {'center_x': .5, 'center_y': .5}
+                        text: 'item 0'
+                        on_release: 
+                        font_size: 13
                         
             
                     
@@ -115,18 +209,6 @@ MDBoxLayout:
             id: three
             title: 'Directorio'
 
-
-<switchVarios>
-    MDLabel:
-        text: "Mismo periodo para los procesos seleccionados"
-        halign: "left"
-        width:150
-        size_hint_x:None
-        font_size: 15
-    
-    MDSwitch:
-        width: dp(64)
-        active: True
    
 
 
@@ -136,8 +218,9 @@ MDBoxLayout:
     
 '''
 
-class switchVarios(MDLabel,MDSwitch):
-    '''Class implementing content for a switch.'''
+
+mes_sel = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+          'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 
 
 class Tab(MDFloatLayout, MDTabsBase):
@@ -149,6 +232,9 @@ class TwoLineIconListItem(TwoLineAvatarIconListItem):
     icon = StringProperty("android")
 
 
+class IconListItem(OneLineIconListItem):
+    icon = StringProperty()
+
 class RightCheckbox(IRightBodyTouch, MDCheckbox):
     '''Custom right container.'''
 
@@ -157,18 +243,109 @@ proceso_estado= [False,False,False,False,False,False,False]
 class Example(MDApp):
     overlay_color = get_color_from_hex("#6042e4")
     
-    
+  
+
+
+
     
     def build(self):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Orange"
         return Builder.load_string(KV)
 
+    def set_item(self, text_item):
+        self.root.ids.drop_item.set_item(text_item)
+        currentDateTime = datetime.datetime.now()
+        date = currentDateTime.date()
+        year = int(date.strftime("%Y"))
+        mes = int(date.strftime("%m"))
+
+        
+        mes_inicial=13
+        mes_ini=13
+        for pro_sgte in range(0,7):
+            match pro_sgte:
+                case 0:
+                    if proceso_estado[pro_sgte]:
+                        if int(text_item) > 2020: mes_inicial=1
+                        elif int(text_item) == 2020: mes_inicial=3
+                        mes_ini=min(mes_ini,mes_inicial)
+                case 1:
+                    if proceso_estado[pro_sgte]:
+                        if int(text_item) == 2019: mes_inicial=5
+                        else: mes_inicial=1
+                        mes_ini=min(mes_ini,mes_inicial)
+                case 2:
+                    if proceso_estado[pro_sgte]:
+                        if int(text_item) == 2021: mes_inicial=4
+                        mes_ini=min(mes_ini,mes_inicial)
+                case 3:
+                    if proceso_estado[pro_sgte]:
+                        if int(text_item) >= 2021: mes_inicial=1
+                        mes_ini=min(mes_ini,mes_inicial)
+
+                case 4:
+                    if proceso_estado[pro_sgte]:
+                         if int(text_item) == 2022: mes_inicial=4
+                         mes_ini=min(mes_ini,mes_inicial)
+
+                case 5:
+                    if proceso_estado[pro_sgte]:
+                        if int(text_item) == 2019: mes_inicial=5
+                        else: mes_inicial=1
+                        mes_ini=min(mes_ini,mes_inicial)
+
+                case 6:
+                    if proceso_estado[pro_sgte]:
+                        if int(text_item) == 2019: mes_inicial=5
+                        else: mes_inicial=1
+                        mes_ini=min(mes_ini,mes_inicial)
+    
+        if int(text_item)<year: mes_fin=12
+        else: mes_fin= mes-1
+
+        menu_items2 = [
+                {
+                    "viewclass": "OneLineListItem",
+                    "text": f"{mes_sel[i-1]}",
+                    "height": dp(56),
+                    "on_release": lambda x=f"{mes_sel[i-1]}": self.set_item2(x),
+                } for i in range(mes_ini,mes_fin+1)
+            ]
+        self.menu2 = MDDropdownMenu(
+                caller= self.root.ids.drop_item2,
+                items=menu_items2,
+                position="center",
+                width_mult=4,
+            )
+        self.menu2.bind()
+
+        #print("Se selecionó el MES "+str(mes))
+        self.menu.dismiss()
+
+    def set_item2(self, text_item):
+        self.root.ids.drop_item2.set_item(text_item)
+        self.menu2.dismiss()
+
+
     def on_start(self):
 
         #self.root.ids.tabs.add_widget(Tab(title=f"Procesos"))
         #self.root.ids.tabs.add_widget(Tab(title=f"Meses"))
         #self.root.ids.tabs.add_widget(Tab(title=f"Directorio"))
+
+
+        self.root.ids.mdlabelano_fn.disabled = True
+        self.root.ids.drop_item_fn.disabled = True
+        self.root.ids.mdlabelmes_fn.disabled = True
+        self.root.ids.drop_item2_fn.disabled = True
+
+        self.root.ids.mdlabelano_fn.opacity = 0
+        self.root.ids.drop_item_fn.opacity = 0
+        self.root.ids.mdlabelmes_fn.opacity = 0
+        self.root.ids.drop_item2_fn.opacity = 0
+
+
 
         icons = list(md_icons.keys())
         #self.root.ids.tabs.add_widget(CalculaCompra(tab_label_text='Calculadora'))
@@ -186,6 +363,10 @@ class Example(MDApp):
         self.root.ids.scroll.selected_mode = True
         self.root.ids.toolbar.right_action_items = [[""]]
         self.root.ids.toolbar.left_action_items = [[""]]
+
+
+
+
     def set_selection_mode(self, instance_selection_list, mode):
         #mode=True
         self.root.ids.scroll.selected_mode = True
@@ -286,33 +467,131 @@ class Example(MDApp):
         #    val_item=ctypes.cast(item, POINTER(kivymd.uix.selection.selection.SelectionItem)).text
         siguientex = " ( "
         i=0
+        anno=2022
         for pro_sgte in range(0,7):
             match pro_sgte:
                 case 0:
-                    if proceso_estado[pro_sgte]: siguientex = siguientex + "  BADX  "
+                    if proceso_estado[pro_sgte]:
+                        siguientex = siguientex + "  BADX  "
+                        i=i+1
+                        if anno > 2020: anno=2020
+
                 case 1:
-                    if proceso_estado[pro_sgte]: siguientex = siguientex + "  BAEN  "
+                    if proceso_estado[pro_sgte]:
+                        siguientex = siguientex + "  BAEN  "
+                        i=i+1
+                        if anno > 2019: anno=2019
+
                 case 2:
-                    if proceso_estado[pro_sgte]: siguientex = siguientex + "  EFAC  "
+                    if proceso_estado[pro_sgte]:
+                        siguientex = siguientex + "  EFAC  "
+                        i=i+1
+                        if anno > 2021: anno=2021
+
                 case 3:
-                    if proceso_estado[pro_sgte]: siguientex = siguientex + "  FETR  "
+                    if proceso_estado[pro_sgte]:
+                        siguientex = siguientex + "  FETR  "
+                        i=i+1
+                        if anno > 2021: anno=2021
+
                 case 4:
-                    if proceso_estado[pro_sgte]: siguientex = siguientex + "  PJDX  "
+                    if proceso_estado[pro_sgte]:
+                        siguientex = siguientex + "  PJDX  "
+                        i=i+1
+                        if anno > 2022: anno=2022
+
                 case 5:
-                    if proceso_estado[pro_sgte]: siguientex = siguientex + "  PMGD  "
+                    if proceso_estado[pro_sgte]:
+                        siguientex = siguientex + "  PMGD  "
+                        i=i+1
+                        if anno > 2019: anno=2019
+
                 case 6:
-                    if proceso_estado[pro_sgte]: siguientex = siguientex + "  RCUT  "
+                    if proceso_estado[pro_sgte]:
+                        siguientex = siguientex + "  RCUT  "
+                        i=i+1
+                        if anno > 2019: anno=2019
         
-        for pro_sgte in range(0,7):
-            if proceso_estado[pro_sgte]: i=i+1
+        #for pro_sgte in range(0,7):
+        #    if proceso_estado[pro_sgte]: i=i+1
+        
 
         print(siguientex+" "+str(i))
 
         if i==1:
             self.root.ids.meses.text = self.root.ids.meses.text+" del proceso seleccionado"+siguientex+" ) para continuar "
+            self.root.ids.variosLB.disabled = True
+            self.root.ids.variosLB.opacity = 0
+            self.root.ids.varios.disabled = True
+            self.root.ids.varios.opacity = 0
         elif i>1:
             self.root.ids.meses.text = self.root.ids.meses.text+" de los procesos seleccionados"+siguientex+" ) para continuar "
-            self.root.ids.gridsw.add_widget(switchVarios())
+            self.root.ids.variosLB.disabled = False
+            self.root.ids.variosLB.opacity = 1
+            self.root.ids.varios.disabled = False
+            self.root.ids.variosLB.opacity = 1
+            self.root.ids.variosLB.text ="Mismo periodo para los "+str(i)+" procesos seleccionados"
+
+        currentDateTime = datetime.datetime.now()
+        date = currentDateTime.date()
+        year = int(date.strftime("%Y"))
+        mes = int(date.strftime("%m"))
+ 
+
+
+        menu_items2 = [
+                {
+                    "viewclass": "OneLineListItem",
+                    "text": f"{mes_sel[i-1]}",
+                    "height": dp(56),
+                    "on_release": lambda x=f"{mes_sel[i-1]}": self.set_item2(x),
+                } for i in range(1,mes)
+            ]
+        self.menu2 = MDDropdownMenu(
+                caller= self.root.ids.drop_item2,
+                items=menu_items2,
+                position="center",
+                width_mult=4,
+            )
+        self.menu2.bind()
+
+
+
+        j=year-anno+1
+        #print(" años a revisar: "+str(j))
+        menu_items = [
+                {
+                    "viewclass": "OneLineListItem",
+                    "text": f"{year-i}",
+                    "height": dp(56),
+                    "on_release": lambda x=f"{year-i}": self.set_item(x),
+                } for i in range(j)
+            ]
+        self.menu = MDDropdownMenu(
+                caller= self.root.ids.drop_item,
+                items=menu_items,
+                position="center",
+                width_mult=4,
+            )
+        self.menu.bind()
+
+        if mes>1:
+
+            self.root.ids.drop_item.text = str(year)
+            self.root.ids.drop_item2.text = mes_sel[mes-2]
+            self.set_item(str(year))
+            self.set_item2(mes_sel[mes-2])
+        else:
+            self.root.ids.drop_item.text = str(year-1)
+            self.root.ids.drop_item2.text = mes_sel[11]
+            self.set_item(str(year-1))
+            self.set_item2(mes_sel[11])
+    
+
+
+
+
+
 
         self.root.ids.tabs.children[0].disabled = False
 
@@ -337,14 +616,86 @@ class Example(MDApp):
             
         #for item in self.root.ids.scroll.children:
         #    if item.text == 'test':
+    def test_state(self):
+
+
+
+        #if mes>1:
+        #    self.root.ids.drop_item.text = str(year)
+        #    self.root.ids.drop_item2.text = mes_sel[mes-2]
+        #else:
+        #    self.root.ids.drop_item.text = str(year-1)
+        #    self.root.ids.drop_item2.text = mes_sel[11]
+
+
+        if self.root.ids.messw.active:
+
+            self.root.ids.mdlabelano_fn.disabled = True
+            self.root.ids.drop_item_fn.disabled = True
+            self.root.ids.mdlabelmes_fn.disabled = True
+            self.root.ids.drop_item2_fn.disabled = True
+
+            self.root.ids.mdlabelano_fn.opacity = 0
+            self.root.ids.drop_item_fn.opacity = 0
+            self.root.ids.mdlabelmes_fn.opacity = 0
+            self.root.ids.drop_item2_fn.opacity = 0
+
+            self.root.ids.mdlabelano.text = "Seleccione el AÑO"
+            self.root.ids.mdlabelmes.text = "y el MES"
+
+        else:
+
+            self.root.ids.mdlabelano.text = "Seleccione el AÑO Inicial"
+            self.root.ids.mdlabelmes.text = "y el MES Inicial"
+
+            self.root.ids.mdlabelano_fn.disabled = False
+            self.root.ids.drop_item_fn.disabled = False
+            self.root.ids.mdlabelmes_fn.disabled = False
+            self.root.ids.drop_item2_fn.disabled = False
+
+            self.root.ids.mdlabelano_fn.opacity = 1
+            self.root.ids.drop_item_fn.opacity = 1
+            self.root.ids.mdlabelmes_fn.opacity = 1
+            self.root.ids.drop_item2_fn.opacity = 1
         
+
+            currentDateTime = datetime.datetime.now()
+            date = currentDateTime.date()
+            year = date.strftime("%Y")
+            mes = int(date.strftime("%m"))
+
+            print(self.root.ids.drop_item2.text)
+            print(mes_sel[mes-2])
+            print(self.root.ids.drop_item.text)
+            print(str(year))
+
+            if self.root.ids.drop_item2.text == mes_sel[mes-2] and self.root.ids.drop_item.text == str(year):
+                self.root.ids.drop_item2.text = mes_sel[mes-3]
+                self.root.ids.drop_item2_fn.text = mes_sel[mes-2]
+            elif mes_sel.index(self.root.ids.drop_item2.text)==11:
+                self.root.ids.drop_item2_fn.text=mes_sel[0]
+            else:
+                self.root.ids.drop_item2_fn.text=mes_sel[mes_sel.index(self.root.ids.drop_item2.text)+1]
+
+
+            if mes_sel.index(self.root.ids.drop_item2.text)==11:
+                self.root.ids.drop_item_fn.text=str(int(self.root.ids.drop_item.text)+1)
+            else:
+                self.root.ids.drop_item_fn.text=self.root.ids.drop_item.text
+
+            print(mes_sel.index(self.root.ids.drop_item2.text))
+            print(self.root.ids.drop_item2.text)
+
         
-    
+
+
+  
+            
 
 #class CalculaCompra(MDFloatLayout, MDTabsBase):
 #    def __init__(self, **kwargs):
 #        super().__init__(**kwargs)
 
-
+      
 
 Example().run()
